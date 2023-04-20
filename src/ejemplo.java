ef3b7c7e64d646f59e43ee2ca2fd2b79e5a78993
alflome <alflome@gmail.com>
2023-04-20 15:10:23 -0600

    clase java ejemplo

---
+++ src/ejemplo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author VG210A18
+ *
+ */
+public class ejemplo {
+	public static void main(String []args) {
+		/**programa sencillo impresion hola curso
+		 * 		
+		 */
+				String saludar = "hola curso desde java";
+				System.out.println(saludar);
+			}
+
+}
